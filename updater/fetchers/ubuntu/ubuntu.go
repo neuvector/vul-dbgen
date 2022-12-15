33a6b26a6026cf91c6f67c5574c1b85b31c94e6b
becitsthere <becitsthere@gmail.com>
2022-12-15 08:10:23 -0800

    Add clair attribution for a few files

--- updater/fetchers/ubuntu/ubuntu.go
+++ updater/fetchers/ubuntu/ubuntu.go
@@ -1,3 +1,17 @@
+// Copyright 2015 clair authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ubuntu
 
 import (
@@ -15,8 +29,8 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/vul-dbgen/common"
+	utils "github.com/vul-dbgen/share"
 	"github.com/vul-dbgen/updater"
-	"github.com/vul-dbgen/share"
 )
 
 const (
