9af69bcd743a6f3c911d9a9906a5c14361face60
acmarr <adam.marr@suse.com>
2024-12-10 16:34:53 -0800

    adjusted openssl fetcher to handle new page format

--- updater/fetchers/apps/apps_test.go
+++ updater/fetchers/apps/apps_test.go
@@ -32,21 +32,16 @@ func TestRubyAffectedVersion(t *testing.T) {
 
 func TestOpensslVulVersion(t *testing.T) {
 	lines := []string{
-		"<ul><li>Fixed in OpenSSL 3.2.2 <a href=\"https://github.com/openssl/openssl/commit/e9d7083e241670332e0443da0f0d4ffb52829f08\">(git commit)</a> (Affected since 3.2.0)</li><li>Fixed in OpenSSL 3.1.6 <a href=\"https://github.com/openssl/openssl/commit/7e4d731b1c07201ad9374c1cd9ac5263bdf35bce\">(git commit)</a> (Affected since 3.1.0)</li><li>Fixed in OpenSSL 3.0.14 <a href=\"https://github.com/openssl/openssl/commit/b52867a9f618bb955bed2a3ce3db4d4f97ed8e5d\">(git commit)</a> (Affected since 3.0.0)</li><li>Fixed in OpenSSL 1.1.1y <a href=\"/support/contracts.html?giturl=https://github.openssl.org/openssl/extended-releases/commit/5f8d25770ae6437db119dfc951e207271a326640\">(premium support)</a> (Affected since 1.1.1)</li></ul>",
-		"<ul><li>Fixed in OpenSSL 0.9.8h (Affected since 0.9.8f)</li></ul>",
-		"<ul><li>Fixed in OpenSSL fips-1.1.2 (Affected since fips-1.1.1)</li></ul>",
-		"<ul><li>Fixed in OpenSSL 0.9.8j (Affected since 0.9.8)</li></ul>",
+		"		<li>from 1.0.1 before 1.0.1u </li>",
+		"<li>from 1.0.2 before 1.0.2i </li>\n<li>from 1.0.4 before 1.0.5d </li>",
 	}
 	affected := [][]common.AppModuleVersion{
 		[]common.AppModuleVersion{
-			{"lt", "3.2.2"}, {"gteq", "3.2.0"}, {"orlt", "3.1.6"}, {"gteq", "3.1.0"},
-			{"orlt", "3.0.14"}, {"gteq", "3.0.0"}, {"orlt", "1.1.1y"}, {"gteq", "1.1.1"}},
+			{"lt", "1.0.1u"}, {"gteq", "1.0.1"},
+		},
 		[]common.AppModuleVersion{
-			{"lt", "0.9.8h"}, {"gteq", "0.9.8f"}},
-		[]common.AppModuleVersion{
-			{"lt", "fips-1.1.2"}, {"gteq", "fips-1.1.1"}},
-		[]common.AppModuleVersion{
-			{"lt", "0.9.8j"}, {"gteq", "0.9.8"}},
+			{"lt", "1.0.2i"}, {"gteq", "1.0.2"}, {"orlt", "1.0.5d"}, {"gteq", "1.0.4"},
+		},
 	}
 
 	for i, line := range lines {
